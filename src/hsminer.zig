e9a2a7431b156aa7ea1cfe4ce21dcf097b72d733
Julien Rouzieres <jrouzieres@scaleway.com>
2025-03-24 13:40:47 +0100

    Embedded files

--- src/hsminer.zig
+++ src/hsminer.zig
@@ -1,8 +1,47 @@
+const c = @cImport({
+    @cInclude("pkcs11zig.h");
+});
 const std = @import("std");
 const zap = @import("zap");
+
+const Mustache = zap.Mustache;
+const template = @embedFile("index.mustache");
 
 pub const HSMiner = struct {
     pub var alloc: std.mem.Allocator = undefined;
 
+    pub var sym: *c.CK_FUNCTION_LIST = undefined;
+
     pub fn on_request(_: zap.Request) void {}
+
+    pub fn render() void {
+        var mustache = Mustache.fromData(template) catch return;
+        defer mustache.deinit();
+
+        const User = struct {
+            name: []const u8,
+            id: isize,
+        };
+
+        const ret = mustache.build(.{
+            .users = [_]User{
+                .{
+                    .name = "Rene",
+                    .id = 1,
+                },
+                .{
+                    .name = "Caro",
+                    .id = 6,
+                },
+            },
+            .nested = .{
+                .item = "nesting works",
+            },
+        });
+        defer ret.deinit();
+
+        if (ret.str()) |s| {
+            std.debug.print("render \"{s}\"\n", .{s});
+        }
+    }
 };
--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,6 @@ const c = @cImport({
 const std = @import("std");
 const HSMiner = @import("hsminer.zig").HSMiner;
 
-var sym: *c.CK_FUNCTION_LIST = undefined;
-
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -20,85 +18,44 @@ pub fn main() anyerror!void {
 
     var getFunctionList: *const fn (**c.CK_FUNCTION_LIST) callconv(.c) c.CK_RV = undefined;
     getFunctionList = dyn_lib.lookup(@TypeOf(getFunctionList), "C_GetFunctionList") orelse return error.LookupFailed;
-    _ = getFunctionList(@ptrCast(&sym));
+    _ = getFunctionList(@ptrCast(&HSMiner.sym));
 
     var args: c.CK_C_INITIALIZE_ARGS = .{ .flags = c.CKF_OS_LOCKING_OK };
-    _ = sym.C_Initialize.?(&args);
+    _ = HSMiner.sym.C_Initialize.?(&args);
 
     var info: c.CK_INFO = undefined;
-    _ = sym.C_GetInfo.?(&info);
+    _ = HSMiner.sym.C_GetInfo.?(&info);
     const manufacturer_id: [32]u8 = info.manufacturerID;
     std.debug.print("info \"{s}\"\n", .{manufacturer_id});
 
     const present: c.CK_BBOOL = c.CK_TRUE;
     var slot_count: c.CK_ULONG = undefined;
-    _ = sym.C_GetSlotList.?(present, null, &slot_count);
+    _ = HSMiner.sym.C_GetSlotList.?(present, null, &slot_count);
     std.debug.print("slot_count {}\n", .{slot_count});
 
     const slot_list = try allocator.alloc(c.CK_ULONG, slot_count);
     errdefer allocator.free(slot_list);
-    _ = sym.C_GetSlotList.?(present, slot_list.ptr, &slot_count);
+    _ = HSMiner.sym.C_GetSlotList.?(present, slot_list.ptr, &slot_count);
 
     var slot_info: c.CK_SLOT_INFO = undefined;
     for (slot_list) |slot| {
-        _ = sym.C_GetSlotInfo.?(slot, &slot_info);
+        _ = HSMiner.sym.C_GetSlotInfo.?(slot, &slot_info);
         const slot_description: [64]u8 = slot_info.slotDescription;
         std.debug.print("slot_info {} \"{s}\"\n", .{ slot, slot_description });
 
         var token_info: c.CK_TOKEN_INFO = undefined;
-        _ = sym.C_GetTokenInfo.?(slot, &token_info);
+        _ = HSMiner.sym.C_GetTokenInfo.?(slot, &token_info);
         const label: [32]u8 = token_info.label;
         const model: [16]u8 = token_info.model;
         std.debug.print("token {} \"{s}\" \"{s}\"\n", .{ (token_info.flags & c.CKF_TOKEN_INITIALIZED) == c.CKF_TOKEN_INITIALIZED, label, model });
     }
 
     var handle: c.CK_SESSION_HANDLE = 0;
-    var r = sym.C_OpenSession.?(slot_list[0], c.CKF_RW_SESSION | c.CKF_SERIAL_SESSION, null, null, &handle);
+    var r = HSMiner.sym.C_OpenSession.?(slot_list[0], c.CKF_RW_SESSION | c.CKF_SERIAL_SESSION, null, null, &handle);
     std.debug.print("session {} {}\n", .{ r == c.CKR_TOKEN_NOT_RECOGNIZED, handle });
 
-    r = sym.C_Login.?(handle, c.CKU_USER, @constCast("1234".ptr), 4);
+    r = HSMiner.sym.C_Login.?(handle, c.CKU_USER, @constCast("1234".ptr), 4);
     std.debug.print("login {}\n", .{r});
-}
 
-const Mustache = @import("zap").Mustache;
-const expect = std.testing.expect;
-
-test "mustache" {
-    const template =
-        \\ {{=<< >>=}}
-        \\ * Users:
-        \\ <<#users>>
-        \\ <<id>>. <<& name>> (<<name>>)
-        \\ <</users>>
-        \\ Nested: <<& nested.item >>.
-    ;
-
-    var mustache = Mustache.fromData(template) catch return;
-    defer mustache.deinit();
-
-    const User = struct {
-        name: []const u8,
-        id: isize,
-    };
-
-    const ret = mustache.build(.{
-        .users = [_]User{
-            .{
-                .name = "Rene",
-                .id = 1,
-            },
-            .{
-                .name = "Caro",
-                .id = 6,
-            },
-        },
-        .nested = .{
-            .item = "nesting works",
-        },
-    });
-    defer ret.deinit();
-
-    if (ret.str()) |s| {
-        try expect(s.len == 65);
-    }
+    HSMiner.render();
 }
