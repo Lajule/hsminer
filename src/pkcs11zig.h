447cba142bb784b757e2318eb5e2b95b183ad455
Julien Rouzieres <jrouzieres@scaleway.com>
2025-04-04 16:53:06 +0200

    Comments

--- src/pkcs11zig.h
+++ src/pkcs11zig.h
@@ -1,3 +1,6 @@
+/* Following macros and defines are required by pkcs11.h, this simplify interop
+ * with Zig.
+ */
 #define CK_PTR *
 #define CK_DEFINE_FUNCTION(returnType, name) returnType name
 #define CK_DECLARE_FUNCTION(returnType, name) returnType name
